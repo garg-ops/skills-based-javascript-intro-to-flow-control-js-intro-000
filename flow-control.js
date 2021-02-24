ca7eda5a072c7099a7f28c0df31509ce27c90f0f
Nilesh Garg <nileshgarg0023@gmail.com>
2021-02-24 13:53:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,7 +27,7 @@ function ternaryTeenager(age) {
   if  (age >= 13 && age <= 19) {
   return ('You are a teenager');
 }else if  (age <=12){
-return ('You are a kid'); }
+return ('You are not a teenager'); }
 
 }
 
