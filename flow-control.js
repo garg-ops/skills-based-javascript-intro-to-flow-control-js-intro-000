0015d7b8812f8f20e26629219436bc1cae4e7311
Nilesh Garg <nileshgarg0023@gmail.com>
2021-02-24 13:57:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,5 +32,9 @@ return ('You are not a teenager'); }
 }
 
 function switchAge(age) {
+  if  (age >= 13 && age <= 19) {
+  return ('You are a teenager');
+} else {
+return ('You have an age'); }
 
 }
